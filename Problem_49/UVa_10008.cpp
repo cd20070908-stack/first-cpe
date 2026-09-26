0130c09144daa5080cd989c8291d59ae1bbc6085
cd20070908-stack <cd20070908@gmail.com>
2026-09-26 23:58:41 +0800

    Update UVa_10008.cpp

--- Problem_49/UVa_10008.cpp
+++ Problem_49/UVa_10008.cpp
@@ -1,27 +1,41 @@
-#include<stdio.h>
-int main(){
-    
-    int a,b;
-    int high,low;
-    
-    while(scanf("%d %d",&a,&b)==2){
-      if(a>b){ high=a;
-               low =b;}
-      else   { high=b;
-               low =a;}
-      int max =0;
-      
-      for (;low<=high;low=low+1){
-          int current =low;
-          int length=1;
-             while(current !=1){
-              if   (current % 2==1){current=3*current+1;}
-              else                 {current=current/2;}
-              length=length+1;
-             }  
-      if (length>max)  {max=length;}
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+
+    int box[26] = {0};
+    int n;
+    scanf("%d", &n);
+    getchar(); //用來吃掉n之後的換行
+    char x;
+    int k;
+    int i;
+    char az[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    int check;
+
+    //統計次數
+    for (i = 0; i < n; i = i + 1) {
+        while ((x = getchar()) != '\n' && x != EOF) { //一字一字讀 //EOF是檔案結束
+            if (isalpha(x) != 0) {                    //是否為英文字母
+                x = toupper(x);                       //轉成大寫
+                for (k = 0; k < 26; k = k + 1) {
+                    if (az[k] == x) {
+                        box[k]++;
+                        break; //加上次數之後跳出for loop
+                    }
+                }
+            }
+        }
     }
-   printf("%d %d %d\n",a,b,max);
- }
-return 0;
+
+    //排序加上輸出
+    for (int z = 1000; z > 0; z = z - 1) { //從1000開始慢慢確認
+        for (check = 0; check < 26; check = check + 1) {
+            if (box[check] == z) {
+                printf("%c %d\n", az[check], box[check]);
+            }
+        }
+    }
+
+    return 0;
 }
